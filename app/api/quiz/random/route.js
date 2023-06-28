10d687c5f1580ec389dfa0320abb06f36957767b
hafsa <tufailhafsa12@gmail.com>
2023-06-28 16:53:06 +0500

    first commit

---
+++ app/api/quiz/random/route.js
@@ -0,0 +1,21 @@
+/**
+ * Retrieves a random question from the quiz.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the ID of a randomly selected question, or an error response.
+ */
+
+import questions from '@/data/quiz.json'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try{
+    const random = Math.floor(Math.random()*questions.data.length)
+    return NextResponse.json({
+      randomQuestions : questions.data[random].id
+    })
+  }
+catch(error){
+  return new NextResponse('Internal Server Error',{status:500})
+
+}
+}
---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+/**
+
+Renders a Next.js page component that displays a grid of character avatars with links to individual character pages.
+@component
+@returns {JSX.Element} The rendered page component.
+*/
+// import {getAllCharacters} from '@lib/characters'
+import { Container } from '@/components'
+import Image from 'next/image'
+import Link from 'next/link'
+import { endpoint } from '@/utils/endpoint'
+
+async function getAllCharacters(){
+  const data = await fetch(`${endpoint}/characters`)
+  if(!data){
+    throw new Error("Failed to fetch data")
+  } 
+   return data.json()
+}
+
+
+export default async function Page() {
+  const data = await getAllCharacters()
+  return (
+    <main className='bg-black'>
+    <Container className='grid grid-col-2 gap-1 py-5 md:grid-cols-3 lg:grid-cols-4'>
+      {data?.characters?.map(item =>{
+        return(
+          <Link href={`/characters/${item.slug}`} key={item.name} className='overflow-hidden rounded-md'>
+               <Image
+                src={item.avatar}
+                alt=""
+                className="transition-all duration-500 hover:scale-110 hover:-rotate-2"
+                width={500}
+                height={500}
+              />
+          </Link>
+
+        )
+      })}
+    </Container>
+    </main>
+  )
+}
